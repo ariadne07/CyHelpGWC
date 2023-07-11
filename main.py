13e1d551ef45c9ec68af11e9ea9f032d0fcaf764
ariadne07 <119911938+ariadne07@users.noreply.github.com>
2023-07-11 12:42:46 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+#CyHelp Starter Code
+
+
+#Greets user
+print("Hello! I'm CyHelp.")
+
+
+#Recounts start of Cybersecurity
+
+
+
+print("The field of Cybersecurity started in the 1970s when more and more information started being stored on computer systems and networks!")
+
+
+#Describes Cybersecurity
+
+
+
+#Introduces CIA Triad
+print("The CIA Triad is the model used to discuss cybersecurity. CIA stands for (add text here)")
+
+
+#Explains pillars of CIA Triad
+print("What would you like to learn more about? Enter the lowercase letter of the following options: (add text here)")
+
+
+#Chatbot ends conversation
+print("Thanks for chatting with me, and I hope you learned something new!")
